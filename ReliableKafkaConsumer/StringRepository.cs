9ae7a291dcc723eb8a5757111b187996c75823bc
Joao Leitao <joao.leitao@farfetch.com>
2021-02-12 12:43:17 +0000

    feat: Draft reliable Confluent.Kafka consumer

---
+++ ReliableKafkaConsumer/StringRepository.cs
@@ -0,0 +1,29 @@
+﻿namespace ReliableKafkaConsumer
+{
+    using System.Collections.Concurrent;
+    using System.Threading.Tasks;
+
+    public class StringRepository
+    {
+        private readonly ConcurrentDictionary<string, string> values;
+
+        public StringRepository()
+        {
+            this.values = new ConcurrentDictionary<string, string>();
+        }
+
+        public Task<string> GetAsync(string key)
+        {
+            return this.values.TryGetValue(key, out var value)
+                ? Task.FromResult(value)
+                : Task.FromResult<string>(null);
+        }
+
+        public Task UpsertAsync(string key, string value)
+        {
+            this.values.AddOrUpdate(key, value, (_, __) => value);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ReliableKafkaProducer/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ReliableKafkaProducer
+{
+    using System;
+    using System.Threading.Tasks;
+    using Confluent.Kafka;
+
+    public static class Program
+    {
+        public static async Task Main()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                Acks = Acks.All,
+            };
+
+            const string Topic = "resilience-tests";
+
+            var producer = new ProducerBuilder<string, string>(config)
+                .SetKeySerializer(Serializers.Utf8)
+                .SetValueSerializer(Serializers.Utf8)
+                .Build();
+
+            for (var index = 0; index < 10000; ++index)
+            {
+                var message = new Message<string, string>
+                {
+                    Key = index.ToString(),
+                    Value = Guid.NewGuid().ToString(),
+                };
+
+                // TODO: create some sort of reliable scenario
+                await producer.ProduceAsync(Topic, message);
+                Console.WriteLine($"Message {index} produced");
+            }
+        }
+    }
+}
